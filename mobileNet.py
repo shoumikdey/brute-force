2ff6959f6b996e5c03634f15099a6cbd125fe89d
Shoumik Dey <shoumikdey123@gmail.com>
2020-10-02 01:43:12 +0530

    Added a image upload and prediction pipeline and changed model to InceptionV3

--- mobileNet.py
+++ mobileNet.py
@@ -2,15 +2,16 @@ import tensorflow as tf
 import numpy as np
 
 
-def getImage(filename):
+def get_image(filename):
     img = tf.keras.preprocessing.image.load_img(filename, target_size=(224, 224))
     img = tf.keras.preprocessing.image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
-    return tf.keras.applications.mobilenet_v2.preprocess_input(img)
+    return tf.keras.applications.inception_v3.preprocess_input(img)
 
 
-model = tf.keras.applications.mobilenet_v2.MobileNetV2()
-test_image = getImage('German_Shepherd.jpg')
-predict = model.predict(test_image)
-results = tf.keras.applications.imagenet_utils.decode_predictions(predict)
-print(results)
+def get_prediction(img):
+    model = tf.keras.applications.inception_v3.InceptionV3()
+    test_image = get_image(img)
+    predict = model.predict(test_image)
+    results = tf.keras.applications.inception_v3.decode_predictions(predict)
+    return results[0]
