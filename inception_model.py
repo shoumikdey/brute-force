c9b21a02bbb2b400fd93bfcc2f616f4f38cccedb
Shoumik Dey <shoumikdey123@gmail.com>
2020-10-03 13:05:11 +0530

    Fixed input image size

--- inception_model.py
+++ inception_model.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def get_image(filename):
-    img = tf.keras.preprocessing.image.load_img(filename, target_size=(224, 224))
+    img = tf.keras.preprocessing.image.load_img(filename, target_size=(299, 299))
     img = tf.keras.preprocessing.image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
     return tf.keras.applications.inception_v3.preprocess_input(img)
